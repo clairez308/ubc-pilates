8fc81d877bb720a1daf6b4ec69b80e3ec5040c9c
Claire Zhang <clairezhang838@gmail.com>
2025-10-01 09:50:27 -0700

    fix deployment error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, {useEffect, useState} from "react";
+// import React, {useEffect, useState} from "react";
 import Link from "next/link";
 
 const Hero = () => {
